55cc7761db032333e554e77378285f559e202e38
peter <zty@fg.pl>
2019-06-08 00:02:23 +0200

    Before amending loging

--- src/main/java/pl/edu/wszib/mojefilmy/controller/FilmDetailsController.java
+++ src/main/java/pl/edu/wszib/mojefilmy/controller/FilmDetailsController.java
@@ -2,6 +2,7 @@ package pl.edu.wszib.mojefilmy.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,15 +11,17 @@ import pl.edu.wszib.mojefilmy.service.BazaFilmowService;
 
 @Controller
 public class FilmDetailsController {
+    @Value("${app.header.pojedynczyfilm}")
+    private String header;
 
     @Autowired
     private BazaFilmowService bazaFilmowService;
 
     @GetMapping("/film/{tytul}")
-    public String getPojedynczyFilmKontroler(@RequestParam(name = "tytul") String nazwa, Model model) {
-        BazaFilmow film = bazaFilmowService.getPojedynczyFilm(nazwa);
+    public String getPojedynczyFilmKontroler(@PathVariable String tytul, Model model) {
+        BazaFilmow film = bazaFilmowService.getPojedynczyFilm(tytul);
         model.addAttribute("film", film);
-//        model.addAttribute("headerPojedynczyKraj", headerPojedynczyKraj);
+        model.addAttribute("header", header);
         return "pojedynczyfilm";
     }
 }
